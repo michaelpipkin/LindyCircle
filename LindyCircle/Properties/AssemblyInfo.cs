31eb1af53802eca8a8ad693c57c38f39de3917ac
mpipkin <michael.a.pipkin@gmail.com>
2019-05-14 16:04:03 -0400

    Update project properties

--- LindyCircle/Properties/AssemblyInfo.cs
+++ LindyCircle/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("The Lindy Circle")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Attendance and finance tracking application")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LindyCircle")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2019 - Michael A Pipkin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
